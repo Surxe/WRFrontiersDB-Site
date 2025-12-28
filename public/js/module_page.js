fe06ea0a7a6a27e45b396961e39456583cce2333
Surxe <119145352+Surxe@users.noreply.github.com>
2025-12-28 15:58:02 -0600

    Initial commit

---
+++ public/js/module_page.js
@@ -0,0 +1,7 @@
+const el = document.getElementById("module-data");
+const module = JSON.parse(el.textContent);
+
+const statsEl = document.getElementById("stats");
+statsEl.innerHTML = `
+  <p>Production_Status: ${module.production_status}</p>
+`;
