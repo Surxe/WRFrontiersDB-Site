b0aacff578727ca052faf9b6146df373e53fb0ca
Surxe <119145352+Surxe@users.noreply.github.com>
2026-01-03 12:27:46 -0600

    Made some module attrs required

--- src/types/module.ts
+++ src/types/module.ts
@@ -1,18 +1,19 @@
 import type { LocalizationKey } from './localization';
 import type { ParseObject } from './parse_object';
-
+// TODO verify which are optional
 export interface Module extends ParseObject {
   production_status?: string;
-  inventory_icon_path?: string;
-  module_rarity_id?: string;
-  character_module_mounts?: Array<{
+  inventory_icon_path: string;
+  module_rarity_id: string;
+  character_module_mounts: Array<{
     character_module_id: string;
     mount: string;
   }>;
   module_tags_ids?: string[];
   name?: LocalizationKey;
   description?: LocalizationKey;
-  module_scalars?: {
+  text_tags?: LocalizationKey[];
+  module_scalars: {
     default_scalars?: Record<string, any>;
     primary_stat_id?: string;
     secondary_stat_id?: string;
@@ -22,8 +23,10 @@ export interface Module extends ParseObject {
     };
     module_name?: string;
   };
-  faction_id?: string;
-  module_classes_ids?: string[];
+  abilities_scalars?: any; // TODO
+  faction_id: string;
+  module_classes_ids: string[];
   module_stats_table_id?: string;
-  module_type_id?: string;
+  module_type_id: string;
+  module_socket_type_ids?: string[];
 }
