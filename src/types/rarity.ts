863d6bdd91be0be896a4d3ae0b2bd6563a5f846d
Surxe <119145352+Surxe@users.noreply.github.com>
2026-01-20 20:51:50 -0600

    Remove rarities url

--- src/types/rarity.ts
+++ src/types/rarity.ts
@@ -1,11 +1,9 @@
 import type { LocalizationKey } from './localization';
 import type { ParseObject } from './parse_object';
 
-export const RARITY_URL = 'rarities';
 
 export interface Rarity extends ParseObject {
   parseObjectClass: 'Rarity';
-  parseObjectUrl: typeof RARITY_URL;
   id: string;
   name: LocalizationKey;
   hex: string;
