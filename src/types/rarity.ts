28e1ca33adc9e048865f087f5c4a1b003cc5ddfc
Surxe <119145352+Surxe@users.noreply.github.com>
2026-01-20 21:17:02 -0600

    lint and format

--- src/types/rarity.ts
+++ src/types/rarity.ts
@@ -1,7 +1,6 @@
 import type { LocalizationKey } from './localization';
 import type { ParseObject } from './parse_object';
 
-
 export interface Rarity extends ParseObject {
   parseObjectClass: 'Rarity';
   id: string;
