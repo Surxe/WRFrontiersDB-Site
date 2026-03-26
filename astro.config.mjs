4443fa6a62726e5016c791a609835d822fedebb5
Surxe <119145352+Surxe@users.noreply.github.com>
2026-03-25 21:25:58 -0500

    Add vercel adapter to support redirects on prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercelStatic from '@astrojs/vercel/static';
 import process from 'node:process';
 
 // Determine base path based on deployment environment
@@ -10,6 +11,7 @@ const isCustomDomain = process.env.CUSTOM_DOMAIN === 'true';
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
+    adapter: vercelStatic(),
     site: isCustomDomain ? 'https://wrf-db.info' : 'https://Surxe.github.io',
     base: '/'
 });
