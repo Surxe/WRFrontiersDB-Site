ade93504f89f34875baf0fc79d740cf6ac0e9697
Surxe <119145352+Surxe@users.noreply.github.com>
2026-04-13 18:51:45 -0500

    CORE_MODULE_CATEGORIES moved to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,3 +9,10 @@ export const PILOT_TYPE_LEGENDARY_REF =
 
 // Template limits
 export const PILOT_TALENT_TEMPLATE_LIMIT = 5;
+
+// Core module category IDs
+export const CORE_MODULE_CATEGORIES = [
+  'DA_ModuleCategory_Chassis.0',
+  'DA_ModuleCategory_Torso.0', 
+  'DA_ModuleCategory_Shoulder.0'
+] as const;
